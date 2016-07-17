7d25da69c5e7bcca947d82530b6f828086020b82
Raymond Maung <raymaung@gmail.com>
2016-07-17 17:30:47 +1000

    chapter 16 - add :push to stack exercise

--- src/ch16/stack.exs
+++ src/ch16/stack.exs
@@ -8,7 +8,15 @@
 defmodule Stack do
   use GenServer
 
+  def handle_call(:pop, _from, []) do
+    { :reply, nil, []}
+  end
+
   def handle_call(:pop, _from, [head | tail]) do
     { :reply, head, tail}
+  end
+
+  def handle_cast({:push, value}, current_stack) do
+    {:noreply, [ value | current_stack ]}
   end
 end
