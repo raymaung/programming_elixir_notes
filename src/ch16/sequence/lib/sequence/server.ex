02c990c16612a6cdb8d497b3797be614a04ebcb8
Raymond Maung <raymaung@gmail.com>
2016-07-17 22:10:20 +1000

    chapter 16 - tidying up

--- src/ch16/sequence/lib/sequence/server.ex
+++ src/ch16/sequence/lib/sequence/server.ex
@@ -13,6 +13,20 @@
 defmodule Sequence.Server do
   use GenServer
 
+  def start_link(current_number) do
+    GenServer.start_link __MODULE__, current_number, name: __MODULE__
+  end
+
+  def next_number do
+    GenServer.call __MODULE__, :next_number
+  end
+
+  def increment_number(delta) do
+    GenServer.cast __MODULE__, {:increment_number, delta}
+  end
+
+  ###########################
+  # GenServer implementation
   def handle_call(:next_number, _from, current_number) do
     { :reply, current_number, current_number + 1}
   end
