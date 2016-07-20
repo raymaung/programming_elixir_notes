d4e328836bd45056dcf13203028ccdb76fc0c676
Raymond Maung <raymaung@gmail.com>
2016-07-20 18:55:12 +1000

    chapter 18 - pass in initial number

--- src/ch18/sequence/lib/sequence.ex
+++ src/ch18/sequence/lib/sequence.ex
@@ -3,11 +3,13 @@ defmodule Sequence do
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
-  def start(_type, _args) do
+  def start(_type, args) do
     import Supervisor.Spec, warn: false
 
+    initial_number = args
+
     children = [
-      worker(Sequence.Supervisor, [123])
+      worker(Sequence.Supervisor, [initial_number])
     ]
 
     opts = [strategy: :one_for_one, name: Sequence.Supervisor]
